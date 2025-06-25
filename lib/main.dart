0734d39c1fb492ca975f7bf386820247fb4711fc
alejandronava562 <navaalejandro562@gmail.com>
2025-06-25 13:26:10 -0700

    building the main dart file, moving onto home screen and login screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'firebase_options.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'firebase_options.dart.';
+import 'screens/login_screen.dart';
+import 'screens/home_screen.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
-
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
@@ -15,21 +19,27 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Firebase App',
-      theme: ThemeData(primarySwatch: Colors.blue),
-      home: const HomePage(),
+      title: '54321 Grocery Planner',
+      theme: ThemeData(primarySwatch: Colors.green),
+      home: AuthGate(),
     );
+    }
   }
-}
 
-class HomePage extends StatelessWidget {
-  const HomePage({super.key});
-
+  class AuthGate extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: const Text('Firebase Connected ✅')),
-      body: const Center(child: Text('You are now connected to Firebase!')),
+    return StreamBuilder<User?>(
+      stream: FirebaseAuth.instance.authStateChanges(),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const Scaffold(body: Center(child: CircularProgressIndicator()));
+        } else if (snapshot.hasData) {
+          return const HomeScreen();
+        } else {
+          return const LoginScreen();
+        }
+      },
     );
   }
 }
