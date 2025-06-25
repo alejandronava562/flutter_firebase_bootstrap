69eea770db62455c369574966550742174d6238e
alejandronava562 <navaalejandro562@gmail.com>
2025-06-25 15:05:17 -0700

    auth gate built

--- lib/auth_gate.dart
+++ lib/auth_gate.dart
@@ -6,6 +6,36 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'screens/home_screen.dart';
 import 'screens/login_screen.dart';
 
+class AuthGate extends StatelessWidget {
+  const AuthGate({super.key});
 
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder<User?>(
+      stream: FirebaseAuth.instance.authStateChanges(),
+      builder: (context, snapshot) {
+        // Loading State
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const Scaffold(
+            body: Center(child: CircularProgressIndicator()),
+          );
+        }
 
+        // Error State
+        if (snapshot.hasError) {
+          return const Scaffold(
+            body: Center(child: Text("Something went wrong")),
+          );
+        }
 
+        // User is signed in
+        if (snapshot.hasData) {
+          return const HomeScreen();
+        }
+
+        // User is not signed in
+        return const LoginScreen();
+      },
+    );
+  }
+}
