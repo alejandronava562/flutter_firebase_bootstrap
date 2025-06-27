c5be7a7a210ae14de9454a4519d66f34f2919b5f
alejandronava562 <navaalejandro562@gmail.com>
2025-06-27 11:09:40 -0700

    user authentication login screen updated

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import '../auth/auth_service.dart';
 
 class LoginScreen extends StatefulWidget {
-  const LoginScreen({super.key})
+  const LoginScreen({super.key});
 
   @override
   State<LoginScreen> createState() => _LoginScreenState();
@@ -10,7 +10,7 @@ class LoginScreen extends StatefulWidget {
 
 class _LoginScreenState extends State<LoginScreen> {
   // final variables
-  final AuthService = _authService = AuthService();
+  final AuthService _authService = AuthService();
   final TextEditingController _emailController = TextEditingController();
   final TextEditingController _passwordController = TextEditingController();
 
@@ -25,14 +25,57 @@ class _LoginScreenState extends State<LoginScreen> {
       _isloading = true;
       _errorMessage = null;
     });
+
+    try {
+      await _authService.signInWithEmail(email, password);
+      // Authgate will auto-redirect
+    } catch (e) {
+      setState(() {
+        _errorMessage = "Login failed: ${e.toString()}";
+      });
+    } finally {
+      setState(() => _isloading = false);
+    }
   }
 
-  
+  void _goToRegister() {
+    Navigator.pushNamed(context, '/register');
+  }
 
-
-  @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    throw UnimplementedError();
+    return Scaffold(
+      appBar: AppBar(title: const Text('Login')),
+      body: Padding(
+        padding: const EdgeInsets.all(16),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            TextField(
+              controller: _emailController,
+              decoration: const InputDecoration(labelText: "Email"),
+              keyboardType: TextInputType.emailAddress,
+            ),
+            const SizedBox(height: 12),
+            TextField(
+              controller: _passwordController,
+              decoration: const InputDecoration(labelText: 'Password'),
+              obscureText: true,
+            ),
+            const SizedBox(height: 20),
+            if (_errorMessage != null)
+            Text(_errorMessage!, style: const TextStyle(color: Colors.red)),
+            _isloading
+              ? const CircularProgressIndicator()
+              : ElevatedButton(
+                onPressed: _login, 
+                child: const Text('Login')
+                ),
+              TextButton(
+                onPressed: _goToRegister, 
+                child: const Text("Don't have an account? Register"),
+                )
+          ],
+        ),)
+    );
   }
 }
