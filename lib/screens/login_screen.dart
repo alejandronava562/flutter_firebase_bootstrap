8e7acdf62eed726b56f80bc6e9d60939290ace17
alejandronava562 <navaalejandro562@gmail.com>
2025-06-27 10:58:20 -0700

    login state functions created

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -9,6 +9,27 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
+  // final variables
+  final AuthService = _authService = AuthService();
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+
+  String? _errorMessage;
+  bool _isloading = false;
+
+  Future<void> _login() async {
+    final email = _emailController.text.trim();
+    final password = _passwordController.text.trim();
+
+    setState(() {
+      _isloading = true;
+      _errorMessage = null;
+    });
+  }
+
+  
+
+
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
