27e4c357aea9373c57e9c876c9fd1b401017c427
alejandronava562 <navaalejandro562@gmail.com>
2025-06-27 10:55:00 -0700

    building user authentication now that the UI is mostly built

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,31 +1,17 @@
 import 'package:flutter/material.dart';
 import '../auth/auth_service.dart';
 
-class LoginScreen extends StatelessWidget {
-  final AuthService _authService = AuthService();
-
-  LoginScreen({super.key});
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({super.key})
 
   @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: const Text("Login")),
-      body: Center(
-        child: ElevatedButton(
-          onPressed: () async {
-            try {
-              await _authService.signInAnonymously();
-              // Once signed in, AuthGate will automatically redirect
-            } catch (e) {
-              print("Anon login error: $e");
-              ScaffoldMessenger.of(context).showSnackBar(
-                SnackBar(content: Text('Failed to sign in anonymously')),
-              );
-            }
-          },
-          child: const Text("Continue as Guest"),
-        ),
-      ),
-    );
+    // TODO: implement build
+    throw UnimplementedError();
   }
 }
